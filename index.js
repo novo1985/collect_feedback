cb6d054ea47f9a5d2e5b461ae53e50197a9f8545
novo1985 <gaolei.ioz@gmail.com>
2017-09-15 16:04:23 -0500

    changed greeting

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ const PORT = process.env.PORT || 5000;
 //whenever HeroKu run the application, it has the ability to inject the variable "env"
 //heroku send us the runtime configuration, only want to tell us the port until the
 //very last second when the app start to excute by Heroku
-
-app.listen(5000);//instruct express to tell Node to listen incoming traffic on port 5000
+app.listen(PORT);//instruct express to tell Node to listen incoming traffic on port 5000
 //test: localhost:5000
 
 
